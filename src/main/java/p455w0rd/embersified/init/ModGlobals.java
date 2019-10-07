3030ca81839d200b3020c273d239e7ba802961de
fnurgel <fnurgel@gmail.com>
2019-10-08 00:03:26 +0200

    fnurg1

--- src/main/java/p455w0rd/embersified/init/ModGlobals.java
+++ src/main/java/p455w0rd/embersified/init/ModGlobals.java
@@ -8,7 +8,7 @@ public class ModGlobals {
 
 	public static final String MODID = "embersified";
 	public static final String NAME = "Embersified";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.3-fnurg1";
 	public static final String DEPENDENCIES = "required-after:embers@[1.8,);";
 	public static final String CONFIG_FILE = NAME + ".cfg";
 
